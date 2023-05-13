76f9912fbe364d53a09e89a77e9dafd267c38404
toomaxhuang <toomax@189.cn>
2023-05-13 22:44:27 +0800

    基于vue2.7+vite+ts+pinia等技术栈开发的博客

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+export const constantRoutes = [
+  {
+    path: '/',
+    name: 'Home',
+    meta: {
+      icon: 'el-icon-setting',
+      title: '首页',
+    },
+    component: () => import('../views/home/index.vue'),
+  },
+]
+
+const createRouter = () =>
+  new VueRouter({
+    routes: constantRoutes,
+  })
+
+const router = createRouter()
+
+export default router
---
+++ src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home-page">
+    Gooderman
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+
+</style>
